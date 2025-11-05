239e8af34743cd9b5d92901713f8b7c7ddee9dcb
nik <nik@fake.com>
2025-11-05 14:10:19 +0000

    change magnification bounds display

--- imagej/src/main/java/com/coxphysics/terrapins/models/frc/Plotter.java
+++ imagej/src/main/java/com/coxphysics/terrapins/models/frc/Plotter.java
@@ -3,12 +3,13 @@ package com.coxphysics.terrapins.models.frc;
 import ij.gui.Plot;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Plotter
 {
 
-    private static final double MIN_MAGNIFICATION = 0.2;
-    private static final double MAX_MAGNIFICATION = 0.4;
+    private static final float MIN_MAGNIFICATION = 0.2f;
+    private static final float MAX_MAGNIFICATION = 0.4f;
 
     public static Plot plot(FRCResult result)
     {
@@ -19,6 +20,7 @@ public class Plotter
     {
         String title = get_title(result.fire_number(), name);
         Plot p = new Plot(title, "q (spatial frequency)", "Correlation");
+        draw_bounds_on(p);
         p.setColor(Color.red);
         double[] frcs = result.get_frcs(0);
         double[] qs = result.qs();
@@ -26,7 +28,6 @@ public class Plotter
         p.setColor(Color.blue);
         double[] threshold_curve = result.threshold_curve();
         p.add("line", qs, threshold_curve);
-        draw_bounds_on(p);
         p.addLegend("Correlation\tThreshold\tMagnification too low\tMagnification too high");
         p.show();
         return p;
@@ -34,17 +35,13 @@ public class Plotter
 
     private static void draw_bounds_on(Plot plot)
     {
-        plot.setColor(Color.black);
-        add_vertical_to(plot, MIN_MAGNIFICATION);
-        plot.setColor(Color.BLACK);
-        add_vertical_to(plot, MAX_MAGNIFICATION);
-    }
-
-    private static void add_vertical_to(Plot plot, double x)
-    {
-        double[] xs = new double[]{x, x};
-        double[] ys = new double[]{0, 1};
-        plot.add("line", xs, ys);
+        float[] corners = new float[]{MIN_MAGNIFICATION, 0.0f, MAX_MAGNIFICATION, 1.0f};
+        ArrayList<float[]> coords = new ArrayList<>();
+        coords.add(corners);
+        plot.drawShapes("rectangles", coords);
+        int plot_object = plot.getNumPlotObjects() - 1;
+        plot.setPlotObjectStyle(plot_object, "black,#A3F7B4");
+        plot.addText("Magnification ok", 0.25, 0.5);
     }
 
     public static String get_title(double fire_number, String name)
