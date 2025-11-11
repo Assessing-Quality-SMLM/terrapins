685708b34e6af71c616adf999770dd732b8ffd57
nik <nik@fake.com>
2025-11-11 13:58:20 +0000

    hook up the find data button

--- imagej/src/main/java/com/coxphysics/terrapins/views/assessment/results/ReportView.java
+++ imagej/src/main/java/com/coxphysics/terrapins/views/assessment/results/ReportView.java
@@ -3,6 +3,39 @@ package com.coxphysics.terrapins.views.assessment.results;
 import com.coxphysics.terrapins.view_models.assessment.ReportVM;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Path;
+
+class FileDialogAction implements ActionListener
+{
+
+    private final ReportView view_;
+
+    private FileDialogAction(ReportView view)
+    {
+        view_ = view;
+    }
+
+    public static FileDialogAction from(ReportView view)
+    {
+        return new FileDialogAction(view);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        JFileChooser jfc = new JFileChooser();
+        jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jfc.setCurrentDirectory(view_.data_path().toFile());
+        int result = jfc.showOpenDialog(null);
+        if (result != JFileChooser.APPROVE_OPTION)
+            return;
+        File current_directory = jfc.getSelectedFile();
+        view_.set_data_path(current_directory.toPath());
+    }
+}
 
 public class ReportView extends JFrame
 {
@@ -16,6 +49,7 @@ public class ReportView extends JFrame
     private ReportView(ReportVM view_model)
     {
         view_model_ = view_model;
+        data_path_btn_.addActionListener(FileDialogAction.from(this));
     }
 
     public static ReportView from(ReportVM view_model)
@@ -24,19 +58,23 @@ public class ReportView extends JFrame
         view.setTitle("Something");
         view.add(view.content_panel_);
         view.reset_data_path();
-        view.display();
         return view;
     }
 
-    private void display()
+    public Path data_path()
     {
-//        content_panel_.setVisible(true);
-//        data_path_.setVisible(true);
+        return view_model_.data_path();
     }
 
     public void reset_data_path()
     {
         data_path_.setText(view_model_.data_path().toString());
 //        pack();
+    }
+
+    public void set_data_path(Path value)
+    {
+        view_model_.set_data_path(value);
+        reset_data_path();
     }
 }
