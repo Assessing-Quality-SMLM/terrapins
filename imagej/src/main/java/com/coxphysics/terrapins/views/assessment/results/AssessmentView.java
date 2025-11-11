e0aa70733331b2b362f0fd2b0d2d8383f753c700
nik <nik@fake.com>
2025-11-11 13:32:35 +0000

    basics of assessment control

--- imagej/src/main/java/com/coxphysics/terrapins/views/assessment/results/AssessmentView.java
+++ imagej/src/main/java/com/coxphysics/terrapins/views/assessment/results/AssessmentView.java
@@ -4,4 +4,9 @@ import javax.swing.*;
 
 public class AssessmentView {
     private JPanel content_panel_;
+    private JTextArea details_;
+    private JLabel name_;
+    private JCheckBox show_results_;
+    private JLabel score_;
+    private JLabel passed_;
 }
