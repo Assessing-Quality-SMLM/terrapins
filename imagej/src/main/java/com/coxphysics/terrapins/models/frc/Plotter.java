35658f1356fa253c34e338d26494cfe54035ddba
Nik <nik@fake.com>
2025-11-14 14:09:07 +0000

    plot calibration data

--- imagej/src/main/java/com/coxphysics/terrapins/models/frc/Plotter.java
+++ imagej/src/main/java/com/coxphysics/terrapins/models/frc/Plotter.java
@@ -1,15 +1,50 @@
 package com.coxphysics.terrapins.models.frc;
 
+import com.coxphysics.terrapins.models.assessment.AssessmentResults;
 import ij.gui.Plot;
+import kotlin.Pair;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.coxphysics.terrapins.models.assessment.AssessmentResultsKt.*;
 
 public class Plotter
 {
 
     private static final float MIN_MAGNIFICATION = 0.2f;
     private static final float MAX_MAGNIFICATION = 0.4f;
+
+    public static Plot plot_sampling_calibration_curves(Pair<String, FRCResult>[] curves)
+    {
+        Map<String, Color> colour_map = new HashMap<String, Color>();
+        colour_map.put(SR_0_005_LABEL, Color.blue);
+        colour_map.put(SR_0_05_LABEL, Color.RED);
+        colour_map.put(SR_0_5_LABEL, Color.GREEN);
+        colour_map.put(SR_5_LABEL, Color.ORANGE);
+        colour_map.put(SR_25_LABEL, Color.MAGENTA);
+        colour_map.put(SR_SAMPLE_LABEL, Color.black);
+        String title = "Sampling Calibration Curves";
+        Plot p = new Plot(title, "q (spatial frequency)", "Correlation");
+        String names = "";
+        for (Pair<String, FRCResult> curve : curves)
+        {
+            FRCResult result = curve.component2();
+            if (result == null)
+                continue;
+            Color colour = colour_map.get(curve.component1());
+            p.setColor(colour);
+            plot_score(result, p);
+            String prefix = names.length() < 1 ? "" : "\t";
+            names = String.format("%s%s%s", names, prefix, curve.component1());
+        }
+
+        p.addLegend(names);
+        p.show();
+        return p;
+    }
 
     public static Plot plot(FRCResult result)
     {
@@ -21,9 +56,7 @@ public class Plotter
         String title = get_title(result.fire_number(), name);
         Plot p = new Plot(title, "q (spatial frequency)", "Correlation");
         p.setColor(Color.red);
-        double[] frcs = result.get_frcs(0);
-        double[] qs = result.qs();
-        p.add("line", qs, frcs);
+        double[] qs = plot_score(result, p);
         p.setColor(Color.blue);
         double[] threshold_curve = result.threshold_curve();
         p.add("line", qs, threshold_curve);
@@ -31,6 +64,14 @@ public class Plotter
         p.addLegend("Correlation\tThreshold\tMagnification too low\tMagnification too high");
         p.show();
         return p;
+    }
+
+    private static double[] plot_score(FRCResult result, Plot p)
+    {
+        double[] qs = result.qs();
+        double[] frcs = result.get_frcs(0);
+        p.add("line", qs, frcs);
+        return qs;
     }
 
     private static void draw_bounds_on(Plot plot)
