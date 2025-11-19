1a862746f388f46d4199f7aa0a9c4d2a23830e99
nik <nik@fake.com>
2025-11-19 11:00:31 +0000

    add the new UI to the workflow

--- imagej/src/main/java/com/coxphysics/terrapins/views/assessment/workflow/Ui.kt
+++ imagej/src/main/java/com/coxphysics/terrapins/views/assessment/workflow/Ui.kt
@@ -20,11 +20,11 @@ private val LOCALISATIONS = "Localisations"
 private val IMAGES = "Images"
 
 class Ui private constructor(
+    private val pre_processing_: PreProcessingUI,
     private val pre_processing_completed_: Checkbox,
-    private val pre_processing_: RadioButtons,
     private val processing_: RadioButtons,
-    private val imagej_preprocessing_ : ImageJPreProcessingUI,
-    private val external_preprocessing_: Message,
+//    private val imagej_preprocessing_ : ImageJPreProcessingUI,
+//    private val external_preprocessing_: Message,
     private val localisation_ui_: AssessmentUI,
     private val images_ui_ : ImagesUI,
     private var reset_images_button_: Button?
@@ -35,18 +35,18 @@ class Ui private constructor(
         @JvmStatic
         fun add_controls_to_dialog(dialog: GenericDialog, settings: Settings): Ui
         {
-            val pre_processing = Utils.add_radio_buttons(dialog, "Pre-processing", arrayOf(IMAGE_J, NOT_IMAGE_J), 2, 1, IMAGE_J)
+            val pre_processing = PreProcessingUI.add_to_dialog(dialog, settings)
 
             val pre_processing_completed = Utils.add_checkbox(dialog, "Super resolution localisations / images created", false)
 
             val processing = Utils.add_radio_buttons(dialog, "Processing", arrayOf(LOCALISATIONS, IMAGES), 2, 1, LOCALISATIONS)
 
-            val imagej_pre_processing = ImageJPreProcessingUI.add_to_dialog(dialog, settings)
-            val external_fitter_ui = Utils.add_message(dialog, "Follow the instructions of your fitter")
+//            val imagej_pre_processing = ImageJPreProcessingUI.add_to_dialog(dialog, settings)
+//            val external_fitter_ui = Utils.add_message(dialog, "Follow the instructions of your fitter")
             val localisation_ui = AssessmentUI.add_controls_to_dialog(dialog, settings.localisation_settings(), false)
             val images_ui = ImagesUI.add_controls_to_dialog(dialog, settings.images_settings(), false)
 
-            val ui = Ui(pre_processing_completed, pre_processing, processing, imagej_pre_processing, external_fitter_ui, localisation_ui, images_ui, null)
+            val ui = Ui(pre_processing, pre_processing_completed, processing, localisation_ui, images_ui, null)
 
             val reset_images_button = Utils.add_button(dialog, "Reset Images", ui)
             ui.reset_images_button_ = reset_images_button
@@ -59,10 +59,11 @@ class Ui private constructor(
     {
         val settings = Settings.default()
 
-        val pre_processing = Utils.extract_radio_buttons(dialog)
+        pre_processing_.create_settings_record(dialog)
+
         val pre_processing_completed = Utils.extract_checkbox_value(dialog)
         val processing = Utils.extract_radio_buttons(dialog)
-        val imagej_preprocessing = imagej_preprocessing_.create_settings_record(dialog)
+//        val imagej_preprocessing = imagej_preprocessing_.create_settings_record(dialog)
 
         val localisation_settings = localisation_ui_.create_settings_record(dialog)
         settings.set_localisations(localisation_settings)
@@ -70,14 +71,13 @@ class Ui private constructor(
         val images_settings = images_ui_.create_settings_record(dialog)
         settings.set_images(images_settings)
 
-
         return settings
 
     }
 
     override fun actionPerformed(e: ActionEvent?)
     {
-        imagej_preprocessing_.reset_images()
+        pre_processing_.reset_images()
         localisation_ui_.reset_images()
         images_ui_.reset_images()
     }
@@ -85,16 +85,20 @@ class Ui private constructor(
     fun handle_event(event: ItemEvent)
     {
         val source = event.source
+        if (pre_processing_.is_event_source(source))
+        {
+            pre_processing_.handle_event(event)
+        }
         if (pre_processing_completed_.is_checkbox(source))
         {
             re_draw_ui()
             return
         }
-        if (pre_processing_.is_button_group(source))
-        {
-            re_draw_ui()
-            return
-        }
+//        if (pre_processing_.is_button_group(source))
+//        {
+//            re_draw_ui()
+//            return
+//        }
         if (processing_.is_button_group(source))
         {
             re_draw_ui()
@@ -127,14 +131,19 @@ class Ui private constructor(
         return processing_.is_checked(IMAGES)
     }
 
-    private fun display_image_j_pre_processing(): Boolean
-    {
-        return pre_processing_enabled() && pre_processing_.is_checked(IMAGE_J)
-    }
+//    private fun display_image_j_pre_processing(): Boolean
+//    {
+//        return pre_processing_enabled() && pre_processing_.is_checked(IMAGE_J)
+//    }
 
-    private fun display_external_pre_processing(): Boolean
+//    private fun display_external_pre_processing(): Boolean
+//    {
+//        return pre_processing_enabled() && pre_processing_.is_checked(NOT_IMAGE_J)
+//    }
+
+    private fun display_pre_processing(): Boolean
     {
-        return pre_processing_enabled() && pre_processing_.is_checked(NOT_IMAGE_J)
+        return !pre_processing_completed()
     }
 
     private fun display_localisation_ui(): Boolean
@@ -152,56 +161,69 @@ class Ui private constructor(
     {
         if (processing_enabled())
         {
-            pre_processing_.set_enabled(false)
+            pre_processing_.set_visibility(false)
             processing_.set_enabled(true)
         }
         else
         {
-            pre_processing_.set_enabled(true)
+            pre_processing_.set_visibility(true)
             processing_.set_enabled(false)
         }
-        if (display_image_j_pre_processing()) {
-            return draw_imagej_ui()
+        if (display_pre_processing())
+        {
+            return draw_pre_processing()
         }
-        if (display_external_pre_processing()) {
-            return draw_external_ui()
-        }
-        if (display_localisation_ui()) {
+//        if (display_image_j_pre_processing()) {
+//            return draw_imagej_ui()
+//        }
+//        if (display_external_pre_processing()) {
+//            return draw_external_ui()
+//        }
+        if (display_localisation_ui())
+        {
             return draw_localisation_ui()
         }
-        if (display_images_ui()) {
+        if (display_images_ui())
+        {
             return draw_images_ui()
         }
     }
 
-    private fun draw_imagej_ui()
+    private fun draw_pre_processing()
     {
-        imagej_preprocessing_.set_visibility(true)
-        external_preprocessing_.set_visibility(false)
+        pre_processing_.set_visibility(true)
         hide_localisation_ui()
         hide_images_ui()
     }
 
-    private fun draw_external_ui()
-    {
-        imagej_preprocessing_.set_visibility(false)
-        external_preprocessing_.set_visibility(true)
-        hide_localisation_ui()
-        hide_images_ui()
-    }
+//    private fun draw_imagej_ui()
+//    {
+//        imagej_preprocessing_.set_visibility(true)
+//        external_preprocessing_.set_visibility(false)
+//        hide_localisation_ui()
+//        hide_images_ui()
+//    }
+//
+//    private fun draw_external_ui()
+//    {
+//        imagej_preprocessing_.set_visibility(false)
+//        external_preprocessing_.set_visibility(true)
+//        hide_localisation_ui()
+//        hide_images_ui()
+//    }
 
     private fun draw_images_ui()
     {
-        imagej_preprocessing_.set_visibility(false)
-        external_preprocessing_.set_visibility(false)
+        pre_processing_.set_visibility(false)
+//        external_preprocessing_.set_visibility(false)
         hide_localisation_ui()
         show_images_ui()
     }
 
     private fun draw_localisation_ui()
     {
-        imagej_preprocessing_.set_visibility(false)
-        external_preprocessing_.set_visibility(false)
+        pre_processing_.set_visibility(false)
+//        external_preprocessing_.set_visibility(false)
         show_localisation_ui()
         hide_images_ui()
     }
