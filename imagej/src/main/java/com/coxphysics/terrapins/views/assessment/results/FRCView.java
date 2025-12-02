9346f4a491de2a0cb75ca83c0e82c595fa8b8732
nik <nik@fake.com>
2025-12-02 14:48:32 +0000

    add a new frc control

---
+++ imagej/src/main/java/com/coxphysics/terrapins/views/assessment/results/FRCView.java
@@ -0,0 +1,8 @@
+package com.coxphysics.terrapins.views.assessment.results;
+
+import javax.swing.*;
+
+public class FRCView {
+    private JCheckBox show_plot_;
+    private JCheckBox show_images_;
+}
