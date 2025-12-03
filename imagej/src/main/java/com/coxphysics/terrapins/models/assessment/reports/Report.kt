2a2366d0c25c173477bdff437d60a92c43a43193
nik <nik@fake.com>
2025-12-03 13:26:54 +0000

    add in  a specialised blinking view for the results

--- imagej/src/main/java/com/coxphysics/terrapins/models/assessment/reports/Report.kt
+++ imagej/src/main/java/com/coxphysics/terrapins/models/assessment/reports/Report.kt
@@ -27,6 +27,7 @@ class Report private constructor(private val results_: AssessmentResults)
     private var drift_view_: ResultsView? = null
 
     private var drift_report_view_: ResultsView? = null
+    private var blinking_report_view_: ResultsView? = null
 
     private var zip_results_ : FRC? = null
     private var zip_view_: ResultsView? = null
@@ -109,14 +110,12 @@ class Report private constructor(private val results_: AssessmentResults)
 
     fun show_blinking_details()
     {
-        zip_view_?.show()
-        drift_view_?.show()
+        blinking_report_view_?.show()
     }
 
     fun hide_blinking_details()
     {
-        zip_view_?.hide()
-        drift_view_?.hide()
+        blinking_report_view_?.hide()
     }
 
     fun sampling_assessment(): Assessment?
@@ -200,14 +199,17 @@ class Report private constructor(private val results_: AssessmentResults)
         half_view_ = half_results?.let{ r -> ResultsView.with(r, "Half Split") }
 
         zip_results_ = results_.zip_split_results()
-        zip_view_ = zip_results_?.results()?.let{ r ->  ResultsView.with(r, "Zip Split") }
+        val zip_results = zip_results_?.results()
+        zip_view_ = zip_results?.let{ r ->  ResultsView.with(r, "Zip Split") }
 
         drift_results_ = results_.drift_split_results()
         val drift_results = drift_results_?.results()
         drift_view_ = drift_results?.let{ r ->  ResultsView.with(r, "Drift Split") }
 
         if (drift_results != null && half_results != null)
-            drift_report_view_ = ResultsView.merged("Drift", "Drift", drift_results, "Half", half_results_!!.results()!!)
+            drift_report_view_ = ResultsView.merged("Drift", "Drift", drift_results, "Half", half_results)
+        if (drift_results != null && zip_results != null)
+            blinking_report_view_ = ResultsView.merged("Blinking", "Drift", drift_results, "Zip", zip_results)
 
         hawkman_results_ = results_.hawkman_results()?.let{ r -> HawkmanResultsView.from(r)}
         squirrel_results_ = results_.squirrel_results()?.let{r -> SquirrelResultsView.from(r)}
