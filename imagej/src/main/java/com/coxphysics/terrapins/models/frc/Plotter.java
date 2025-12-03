33f80376db35dc9834ec048503affbfa7916f5ff
nik <nik@fake.com>
2025-12-03 10:52:35 +0000

    remove unsued

--- imagej/src/main/java/com/coxphysics/terrapins/models/frc/Plotter.java
+++ imagej/src/main/java/com/coxphysics/terrapins/models/frc/Plotter.java
@@ -1,11 +1,9 @@
 package com.coxphysics.terrapins.models.frc;
 
-import com.coxphysics.terrapins.models.assessment.AssessmentResults;
 import ij.gui.Plot;
 import kotlin.Pair;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
