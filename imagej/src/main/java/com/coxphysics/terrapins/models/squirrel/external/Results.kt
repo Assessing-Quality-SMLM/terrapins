7053aa29b94b6bfcf5716639c0d5a292f3f76edd
Nik <nik@fake.com>
2025-12-08 12:24:18 +0000

    model all parts of squirrel results

--- imagej/src/main/java/com/coxphysics/terrapins/models/squirrel/external/Results.kt
+++ imagej/src/main/java/com/coxphysics/terrapins/models/squirrel/external/Results.kt
@@ -1,8 +1,9 @@
 package com.coxphysics.terrapins.models.squirrel.external
 
 import com.coxphysics.terrapins.models.squirrel.utils.StackHelper
+import com.coxphysics.terrapins.models.utils.FsUtils
 import com.coxphysics.terrapins.models.utils.IJUtils
-import ij.IJ
+import ij.ImagePlus
 import java.nio.file.Path
 
 class Results private constructor(private val data_path_: Path)
@@ -21,18 +22,32 @@ class Results private constructor(private val data_path_: Path)
         return data_path_.resolve("error_map.tiff")
     }
 
-    fun display_error_map()
+    private fun optimiser_data_path(): Path
+    {
+        return data_path_.resolve("optimiser_data")
+    }
+
+    fun load_error_map(): ImagePlus?
     {
         val map = IJUtils.load_image(error_map_path())
         if (map == null)
-            return
+            return null
         StackHelper.applyLUT(map,"SQUIRREL-Errors.lut");
-        map.show()
+        return map
     }
 
+    fun display_error_map()
+    {
+        load_error_map()?.show()
+    }
 
     fun display()
     {
         display_error_map()
+    }
+
+    fun optimiser_output(): String?
+    {
+        return FsUtils.read_to_string_utf8(optimiser_data_path())
     }
 }
