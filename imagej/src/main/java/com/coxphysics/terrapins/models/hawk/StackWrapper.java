d56e6a2de6d39cb5e603d3a8741cc00aa5b8addb
nik <nik@fake.com>
2025-12-09 15:37:35 +0000

    handle the data being deleted despite holding a reference

--- imagej/src/main/java/com/coxphysics/terrapins/models/hawk/StackWrapper.java
+++ imagej/src/main/java/com/coxphysics/terrapins/models/hawk/StackWrapper.java
@@ -1,5 +1,6 @@
 package com.coxphysics.terrapins.models.hawk;
 
+import ij.IJ;
 import ij.ImageStack;
 import ij.process.ImageProcessor;
 
@@ -27,8 +28,20 @@ public class StackWrapper
     // Used by backend in jni land
     public float[] get_float_frame(int frame)
     {
-        ImageProcessor float_frame = stack_.getProcessor(frame + 1).convertToFloat();
-        return (float[]) float_frame.getPixels();
+        // If the underlying image stack window is closed it deletes all the data - even if you are holding a reference
+        // But it retains size info so cannot check by frame number - just have to catch the exception
+        // because size info is retained we can use it to return a big block of zero
+        try
+        {
+            ImageProcessor processor = stack_.getProcessor(frame + 1);
+            ImageProcessor float_frame = processor.convertToFloat();
+            return (float[]) float_frame.getPixels();
+        }
+        catch (IllegalArgumentException e)
+        {
+            IJ.log(String.format("Somethings gone wrong: %s. Have you closed your underlying image? If so don't it needs to be open", e));
+            return new float[stack_.getHeight() * stack_.getWidth()];
+        }
     }
 
 
