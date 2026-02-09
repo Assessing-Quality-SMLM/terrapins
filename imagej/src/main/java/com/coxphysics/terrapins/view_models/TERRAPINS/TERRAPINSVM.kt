8df4e7995a37c2c22560775ad5c71d2f9d0d836f
Nik <nik@fake.com>
2026-02-09 11:40:48 +0000

    hook in the image view

--- imagej/src/main/java/com/coxphysics/terrapins/view_models/TERRAPINS/TERRAPINSVM.kt
+++ imagej/src/main/java/com/coxphysics/terrapins/view_models/TERRAPINS/TERRAPINSVM.kt
@@ -6,6 +6,7 @@ class TERRAPINSVM private constructor(private val settings_: Settings)
 {
     private val pre_processing_vm_: PreProcessingVM = PreProcessingVM.from(settings_)
     private val localisation_vm_ : LocalisationVM = LocalisationVM.from(settings_.localisation_settings())
+    private val images_vm_ : ImagesVM = ImagesVM.from(settings_.images_settings())
     companion object
     {
         @JvmStatic
@@ -29,5 +30,20 @@ class TERRAPINSVM private constructor(private val settings_: Settings)
     fun localisation_vm(): LocalisationVM
     {
         return localisation_vm_
+    }
+
+    fun images_vm(): ImagesVM
+    {
+        return images_vm_
+    }
+
+    fun use_localisations(): Boolean
+    {
+        return settings_.use_localisations()
+    }
+
+    fun set_use_localisations(value: Boolean)
+    {
+        settings_.set_use_localisations(value)
     }
 }
