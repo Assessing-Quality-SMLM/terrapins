f7422baa70e0e607fa6b6b846be57b60acf346ee
Nik <nik@fake.com>
2026-02-12 11:04:34 +0000

    set and expose the labels

--- imagej/src/main/java/com/coxphysics/terrapins/view_models/TERRAPINS/JointImagesVM.kt
+++ imagej/src/main/java/com/coxphysics/terrapins/view_models/TERRAPINS/JointImagesVM.kt
@@ -2,7 +2,11 @@ package com.coxphysics.terrapins.view_models.TERRAPINS
 
 import com.coxphysics.terrapins.models.io.JointImages
 
-class JointImagesVM private constructor(private val model_: JointImages)
+class JointImagesVM private constructor(
+    private val model_: JointImages,
+    private val label_a_: String,
+    private val label_b_: String
+)
 {
     private var image_1_ = DiskOrImageVM.from(model_.image_1())
     private var image_2_ = DiskOrImageVM.from(model_.image_2())
@@ -10,15 +14,15 @@ class JointImagesVM private constructor(private val model_: JointImages)
     companion object
     {
         @JvmStatic
-        fun from(model: JointImages): JointImagesVM
+        fun from(model: JointImages, label_a: String, label_b: String): JointImagesVM
         {
-            return JointImagesVM(model)
+            return JointImagesVM(model, label_a, label_b)
         }
 
         @JvmStatic
         fun default(): JointImagesVM
         {
-            return from(JointImages.default())
+            return from(JointImages.default(), "Image a", "Image b")
         }
 
         // for calls from Java
@@ -29,9 +33,19 @@ class JointImagesVM private constructor(private val model_: JointImages)
         }
     }
 
+    fun image_a_label(): String
+    {
+        return label_a_
+    }
+
     fun image_1_vm(): DiskOrImageVM
     {
         return image_1_
+    }
+
+    fun image_b_label(): String
+    {
+        return label_b_
     }
 
     fun image_2_vm(): DiskOrImageVM
