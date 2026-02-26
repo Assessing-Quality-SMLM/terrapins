4692e2ff75c1abacd16b9da7740569f4a833996d
Nik <nik@fake.com>
2026-02-26 15:21:14 +0000

    make sure is ST

--- imagej/src/test/java/com/coxphysics/terrapins/models/hawk/SettingsTests.kt
+++ imagej/src/test/java/com/coxphysics/terrapins/models/hawk/SettingsTests.kt
@@ -6,7 +6,10 @@ import com.coxphysics.terrapins.models.utils.StringUtils
 import com.coxphysics.terrapins.plugins.*
 import ij.ImagePlus
 import ij.process.FloatProcessor
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 
 class FakeWindowManager private constructor(
@@ -35,6 +38,15 @@ class FakeWindowManager private constructor(
 
 class SettingsTests
 {
+    private var executor = Executors.newSingleThreadExecutor();
+
+    @AfterEach
+    fun cleanUp()
+    {
+        executor.shutdown();
+        while (!executor.awaitTermination(100, TimeUnit.MICROSECONDS));
+    }
+
     @Test
     fun can_parse_image_name()
     {
@@ -124,31 +136,33 @@ class SettingsTests
     @Test
     fun round_trip_macro_io()
     {
-        val settings = Settings.default()
-        val image = ImagePlus("an_image", FloatProcessor(1, 1))
-        settings.set_image(image)
-        settings.set_filename("something")
-        settings.set_n_levels(100)
-        settings.set_output_style(OutputStyle.TEMPORAL)
-        settings.set_negative_handling(NegativeValuesPolicy.SEPARATE)
-        settings.record_values()
+        executor.submit {
+            val settings = Settings.default()
+            val image = ImagePlus("an_image", FloatProcessor(1, 1))
+            settings.set_image(image)
+            settings.set_filename("something")
+            settings.set_n_levels(100)
+            settings.set_output_style(OutputStyle.TEMPORAL)
+            settings.set_negative_handling(NegativeValuesPolicy.SEPARATE)
+            settings.record_values()
 
-        val options = MacroOptions.from_recorder_command_options()
-        assertEquals(options.get(HAWK_IMAGE_NAME), "an_image")
-        assertEquals(options.get(HAWK_SAVE_TO_DISK), "something")
-        assertEquals(options.get(HAWK_N_LEVELS), "100")
-        assertEquals(options.get(HAWK_OUTPUT_STYLE), "temporal")
-        assertEquals(options.get(HAWK_NEGATIVE_VALUES), "separate")
+            val options = MacroOptions.from_recorder_command_options()
+            assertEquals(options.get(HAWK_IMAGE_NAME), "an_image")
+            assertEquals(options.get(HAWK_SAVE_TO_DISK), "something")
+            assertEquals(options.get(HAWK_N_LEVELS), "100")
+            assertEquals(options.get(HAWK_OUTPUT_STYLE), "temporal")
+            assertEquals(options.get(HAWK_NEGATIVE_VALUES), "separate")
 
-        val window_manager = FakeWindowManager.from(mapOf(Pair("an_image", image)))
-        val new_settings = Settings.from_macro_options_with(options, window_manager)
-        assertEquals(new_settings.image(), image)
-        assertEquals(new_settings.filename(), settings.filename())
-        assertEquals(new_settings.n_levels(), settings.n_levels())
-        assertEquals(new_settings.is_absolute(), settings.is_absolute())
-        assertEquals(new_settings.is_separate(), settings.is_separate())
-        assertEquals(new_settings.is_temporal(), settings.is_temporal())
-        assertEquals(new_settings.is_sequential(), settings.is_sequential())
+            val window_manager = FakeWindowManager.from(mapOf(Pair("an_image", image)))
+            val new_settings = Settings.from_macro_options_with(options, window_manager)
+            assertEquals(new_settings.image(), image)
+            assertEquals(new_settings.filename(), settings.filename())
+            assertEquals(new_settings.n_levels(), settings.n_levels())
+            assertEquals(new_settings.is_absolute(), settings.is_absolute())
+            assertEquals(new_settings.is_separate(), settings.is_separate())
+            assertEquals(new_settings.is_temporal(), settings.is_temporal())
+            assertEquals(new_settings.is_sequential(), settings.is_sequential())
+        }.get()
 
     }
 }
