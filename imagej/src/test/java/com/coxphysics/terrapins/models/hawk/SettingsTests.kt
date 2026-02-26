8f621b6f745b13b64122e0dd4c4854f84d9046f3
Nik <nik@fake.com>
2026-02-26 15:19:45 +0000

    add a round trip test

--- imagej/src/test/java/com/coxphysics/terrapins/models/hawk/SettingsTests.kt
+++ imagej/src/test/java/com/coxphysics/terrapins/models/hawk/SettingsTests.kt
@@ -3,6 +3,7 @@ package com.coxphysics.terrapins.models.hawk
 import com.coxphysics.terrapins.models.ij_wrapping.WindowManager
 import com.coxphysics.terrapins.models.macros.MacroOptions
 import com.coxphysics.terrapins.models.utils.StringUtils
+import com.coxphysics.terrapins.plugins.*
 import ij.ImagePlus
 import ij.process.FloatProcessor
 import org.junit.jupiter.api.Test
@@ -118,5 +119,36 @@ class SettingsTests
         val options = MacroOptions.from("save_to_disk=something")
         val settings = Settings.from_macro_options(options)
         assertEquals(settings.filename(), "something")
+    }
+
+    @Test
+    fun round_trip_macro_io()
+    {
+        val settings = Settings.default()
+        val image = ImagePlus("an_image", FloatProcessor(1, 1))
+        settings.set_image(image)
+        settings.set_filename("something")
+        settings.set_n_levels(100)
+        settings.set_output_style(OutputStyle.TEMPORAL)
+        settings.set_negative_handling(NegativeValuesPolicy.SEPARATE)
+        settings.record_values()
+
+        val options = MacroOptions.from_recorder_command_options()
+        assertEquals(options.get(HAWK_IMAGE_NAME), "an_image")
+        assertEquals(options.get(HAWK_SAVE_TO_DISK), "something")
+        assertEquals(options.get(HAWK_N_LEVELS), "100")
+        assertEquals(options.get(HAWK_OUTPUT_STYLE), "temporal")
+        assertEquals(options.get(HAWK_NEGATIVE_VALUES), "separate")
+
+        val window_manager = FakeWindowManager.from(mapOf(Pair("an_image", image)))
+        val new_settings = Settings.from_macro_options_with(options, window_manager)
+        assertEquals(new_settings.image(), image)
+        assertEquals(new_settings.filename(), settings.filename())
+        assertEquals(new_settings.n_levels(), settings.n_levels())
+        assertEquals(new_settings.is_absolute(), settings.is_absolute())
+        assertEquals(new_settings.is_separate(), settings.is_separate())
+        assertEquals(new_settings.is_temporal(), settings.is_temporal())
+        assertEquals(new_settings.is_sequential(), settings.is_sequential())
+
     }
 }
