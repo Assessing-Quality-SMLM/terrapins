61fa5170d1e70fcbe28feb6ef4ce55c555c50295
Nik <nik@fake.com>
2026-07-24 15:52:28 +0100

    set the y bounds on the frc plots

--- imagej/src/test/java/com/coxphysics/terrapins/models/frc/PlotterTests.java
+++ imagej/src/test/java/com/coxphysics/terrapins/models/frc/PlotterTests.java
@@ -19,4 +19,18 @@ public class PlotterTests
         String title = Plotter.get_title(1.1, null);
         assertEquals(title, "FIRE: 1.10");
     }
+
+    @Test
+    public void y_min_on_frc_plots_floors_to_0()
+    {
+        double[] data = {1.0, 0.5, 3.0};
+        assertEquals(Plotter.get_y_min(data), 0.0);
+    }
+
+    @Test
+    public void y_min_on_frc_plots_can_be_negative()
+    {
+        double[] data = {1.0, 0.5, -3.0};
+        assertEquals(Plotter.get_y_min(data), -3.0);
+    }
 }
